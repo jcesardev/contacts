72357515b8387d1cbfab0f409b0aeb8440bfd9cb
Julio Cesar Bolaños Palacios <jcesar.linux@gmail.com>
2019-01-23 22:36:08 -0600

    Swagger UI integration.

--- src/main/java/com/albo/directory/DirectoryApplication.java
+++ src/main/java/com/albo/directory/DirectoryApplication.java
@@ -3,8 +3,11 @@ package com.albo.directory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
+@EnableSwagger2
 public class DirectoryApplication{          
 	public static void main(String[] args) {
 		SpringApplication.run(DirectoryApplication.class, args);
